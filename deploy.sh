1c2be21940483feae61a86b53ef234139cdd6732
adrianppg <adrianppg@gmail.com>
2019-02-28 03:06:33 +0100

    Solving stuff

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo -e "\033[0;32mDeploying code updates to GitHub...\033[0m"
+
+# Add changes to git.
+git add .
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+############################################
+echo -e "\033[0;32mDeploying public Website updates to GitHub...\033[0m"
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
+
+# Commit changes.
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
+# Come Back up to the Project Root
+cd ..
+
+hugo server
